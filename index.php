78c1cf5e5611c86caaa7602d98092bdeac47109b
Philip Meissner <phil@twopaperdolls.com>
2011-10-13 16:41:07 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Two PaperDolls Welcome</title>
+<style type="text/css">
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, font, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	outline: 0;
+	font-weight: inherit;
+	font-style: inherit;
+	font-size: 100%;
+	font-family: inherit;
+	vertical-align: baseline;
+}
+:focus {
+	outline: 0;
+}
+body {
+	line-height: 1;
+	color: black;
+	background: white;
+}
+ol, ul {
+	list-style: none;
+}
+table {
+	border-collapse: separate;
+	border-spacing: 0;
+}
+body { width: 520px; overflow:hidden; }
+a { display: block; }
+</style>
+</head>
+<body>
+	<img src="images/show_some_love.jpg" alt="A letterpress design that reads show us some love recommend us" width="520px" height="269" />
+    <a href="http://www.etsy.com/shop/TwoPaperdollsShop?section_id=10476644&view_type=gallery" target="_blank" title="Shop Two Paperdolls Holiday Cards" style="width: 520px; height: 375px;">
+    	<img src="images/holiday_cards.jpg" alt="Letterpress holiday cards" width="520px" height="375" />
+    </a>
+</body>
+</html>
